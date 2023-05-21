82b0f3fbd20df2ec04ad9ae33c15108185f3e4e4
JeremieG06300 <jeremie.garod@epitech.eu>
2023-05-21 16:34:32 +0200

    ajout commentaires code html , css et js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+// Sélectionner l'élément avec la classe "dice"
 const dice = document.querySelector('.dice');
+
+// Sélectionner l'élément avec la classe "roll"
 const rollBtn = document.querySelector('.roll');
 
+// Fonction pour générer un nombre aléatoire et faire rouler le dé
 const randomDice = () => {
-
+  // Générer un nombre aléatoire entre 0 et 9
   const random = Math.floor(Math.random() * 10);
 
+  // Vérifier si le nombre est entre 1 et 6 (inclus)
   if (random >= 1 && random <= 6) {
+    // Appeler la fonction pour faire rouler le dé avec le nombre aléatoire
     rollDice(random);
   } else {
+    // Si le nombre n'est pas entre 1 et 6, rappeler la fonction pour générer un autre nombre aléatoire
     randomDice();
   }
 };
 
+// Fonction pour faire rouler le dé avec une animation
 const rollDice = (random) => {
+  // Ajouter l'animation "rolling" au dé pendant 4 secondes
   dice.style.animation = "rolling 4s";
 
+  // Après 4 secondes (4050 millisecondes), effectuer les transformations appropriées en fonction du nombre aléatoire
   setTimeout(() => {
-
     switch (random) {
       case 1:
         dice.style.transform = "rotateX(0deg) rotateY(0deg)";
@@ -46,9 +55,11 @@ const rollDice = (random) => {
         break;
     }
 
+    // Retirer l'animation du dé
     dice.style.animation = 'none';
 
   }, 4050);
 };
 
+// Ajouter un écouteur d'événement au bouton "roll" pour déclencher la fonction randomDice lorsqu'il est cliqué
 rollBtn.addEventListener("click", randomDice);
